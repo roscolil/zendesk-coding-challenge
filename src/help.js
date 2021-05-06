78f4a54598f808fbfa927cc6749e4e84de0ae19b
Ross <ross.lillis@gmail.com>
2021-05-06 15:41:25 +1000

    feat: build app menu commands

--- src/help.js
+++ src/help.js
@@ -3,10 +3,19 @@ import chalk from 'chalk'
 const menus = {
   main: `
   ${chalk.blueBright('Zendesk CLI search application')}
-  ${chalk.blueBright('Search by following the below command line format')}
-  ${chalk.blueBright('----------------------------------------------------\n')}
-  ${chalk.greenBright('search [search string]')}
-  `
+  ${chalk.blueBright('Query the JSON data by using the following command line format below')}
+  ${chalk.blueBright('--------------------------------------------------------------------\n')}
+  ${chalk.greenBright('zendesk [command] <search string>\n')} 
+  ${chalk.greenBright('E.g. $ zendesk search megacorp\n')} 
+  ${chalk.redBright('Commands:')}
+  ${chalk.redBright('search')} ...................... to search JSON data
+  ${chalk.redBright('fields')} ...................... to view a list of searchable fields
+  ${chalk.redBright('version')} ..................... to show app version
+  `,
+  // TODO may need a quit option in menu otherwise delete
+  search: `search()`,
+  viewfields: `viewFields()`
+  // TODO add search help function here??
 }
 
 export async function help(args) {
